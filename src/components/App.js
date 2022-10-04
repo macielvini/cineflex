80525454122b8f82623cf0ad5e1653b4b7dee7c0
VInicius Maciel <vm.viniciusmaciel@gmail.com>
2022-10-04 15:06:59 -0300

    Header styled component finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "../GlobalStyles";
+
 export default function App() {
-  return <>"oi"</>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledHeader>CINEFLEX</StyledHeader>
+    </>
+  );
 }
+
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 67px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #c3cfd9;
+
+  color: #e8833a;
+  font-size: 32px;
+  font-weight: 700;
+  text-align: center;
+`;
